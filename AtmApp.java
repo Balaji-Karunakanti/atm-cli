7ad135a2a82910326a13eabf6b40da0401a98307
Balaji <balajikarunakanti@gmail.com>
2025-12-18 18:45:25 +0530

    added few functionalities

---
+++ AtmApp.java
@@ -0,0 +1,8 @@
+package AtmCli;
+
+public class AtmApp{
+   public static void main(String[] args) {
+    Account acc = new Account(102116212116l, 200);
+    
+   }
+}
--- MenuUtil.java
+++ MenuUtil.java
@@ -1,5 +1,27 @@
 package AtmCli;
-
+import java.util.*;
 public class MenuUtil {
-  
+
+  Scanner scan = new Scanner(System.in);
+   public void displayWelcomeMessage(){
+    System.out.println("ATM system start");
+   }
+
+   public void displayMainMenu(){
+    System.out.println("1.Check Balance:");
+    System.out.println("2.Deposit:");
+    System.out.println("3.Withdraw:");
+    System.out.println("4.Exit:");
+   }
+
+  public int getUserChoice(){
+    System.out.println("Enter your choice:");
+    int choice = scan.nextInt();
+    return choice;
+  }
+
+   public void displayExitMessage(){
+    System.out.println("Thank-you");
+   }
+
 }
